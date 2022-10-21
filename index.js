a7e7c4e6fdbd9f6f7f60bd07e557f7ae734697aa
OldDog-Z124 <OldDog.Z124@qq.com>
2022-10-21 14:39:54 +0800

    add build

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ morgan.token('type', (req, res) => {
 
 const app = express()
 
+app.use(express.static('build'))
 app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :type'))
