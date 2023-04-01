2a529eb851bfbdad81b7e9daf053f34f92b81f84
OldDog-Z124 <OldDog.Z124@qq.com>
2023-04-01 22:16:04 +0800

    add mongodb demo

---
+++ mongo.js
@@ -0,0 +1,49 @@
+require('dotenv').config()
+
+const mongoose = require('mongoose')
+
+const password = process.argv?.[2]  
+const url = 
+  `mongodb+srv://olddogz124:${password}@cluster0.2zfeqxk.mongodb.net/phonebookApp?retryWrites=true&w=majority`
+
+mongoose.set('strictQuery', false)
+mongoose.connect(url) 
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if (process.argv.length === 5) {
+  const name = process.argv[3]
+  const number = process.argv[4]
+
+  const person = new Person({
+    name,
+    number
+  })
+
+  person.save()
+    .then(person => {
+      console.log(`added ${person.name} number ${person.number} to phonebook`)
+      console.log('-- --- --')
+      mongoose.connection.close()
+    })
+}
+else if (process.argv.length === 3) {
+  Person.find({})
+    .then(persons => {
+      console.log('phonebook:')
+      persons.forEach(person => {
+        console.log(`${person.name} ${person.number}`)
+      })
+      console.log('-- --- --')
+      mongoose.connection.close()
+    })
+}
+else {
+  mongoose.connection.close()
+}
+
